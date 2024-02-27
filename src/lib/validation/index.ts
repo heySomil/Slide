dd36cefc01071ee1da473c1cfb52a3b9b49cf410
heySomil <avi.s2403@gmail.com>
2024-02-27 11:34:02 +0530

    create post

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -12,3 +12,10 @@ export const SignupValidation = z.object({
     password: z.string().min(8, {message:"Password must be atleat 8 characters"}),
   })
   
+  export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
+  })
+  
